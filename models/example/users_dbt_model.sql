930033c8691cd30755e8449ac747bb92ea8ab9b7
Shrikanth <shrikanth_kr@yahoo.com>
2024-05-25 15:34:47 +0530

    remove packages yaml and use only scalar

--- models/example/users_dbt_model.sql
+++ models/example/users_dbt_model.sql
@@ -2,17 +2,17 @@
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
 
 select
-    {{ json_extract_scalar('_airbyte_data', ['_id']) }} as legacy_id,
-    {{ json_extract_scalar('_airbyte_data', ['createdAt']) }} as start_date,
-    {{ json_extract_scalar('_airbyte_data', ['updatedAt']) }} as update_date,
-    {{ json_extract_scalar('_airbyte_data', ['name']) }} as name,
-    {{ json_extract_scalar('_airbyte_data', ['email']) }} as email,
-    {{ json_extract_scalar('_airbyte_data', ['status']) }} as status,
-    {{ json_extract_scalar('_airbyte_data', ['lastName']) }} as last_name,
-    {{ json_extract_scalar('_airbyte_data', ['firstName']) }} as first_name,
-    {{ json_extract_scalar('_airbyte_data', ['remoteCreatedAt']) }} as remote_created_at,
-    {{ json_extract_scalar('_airbyte_data', ['employmentStatus']) }} as employment_status,
-    {{ json_extract_scalar('_airbyte_data', ['organization']) }} as organization,
-    {{ json_extract_scalar('_airbyte_data', ['onboardingStep']) }} as onboarding_step,
+    {{ json_extract('_airbyte_data', ['_id']) }} as legacy_id,
+    {{ json_extract('_airbyte_data', ['createdAt']) }} as start_date,
+    {{ json_extract('_airbyte_data', ['updatedAt']) }} as update_date,
+    {{ json_extract('_airbyte_data', ['name']) }} as name,
+    {{ json_extract('_airbyte_data', ['email']) }} as email,
+    {{ json_extract('_airbyte_data', ['status']) }} as status,
+    {{ json_extract('_airbyte_data', ['lastName']) }} as last_name,
+    {{ json_extract('_airbyte_data', ['firstName']) }} as first_name,
+    {{ json_extract('_airbyte_data', ['remoteCreatedAt']) }} as remote_created_at,
+    {{ json_extract('_airbyte_data', ['employmentStatus']) }} as employment_status,
+    {{ json_extract('_airbyte_data', ['organization']) }} as organization,
+    {{ json_extract('_airbyte_data', ['onboardingStep']) }} as onboarding_step,
     _airbyte_emitted_at
 from 'public._airbyte_raw_users'
